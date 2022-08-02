c01a17c65ad7a59b9b4b1d67549d93d746a12986
lks2007 <lukas.soigneux@gmail.com>
2022-08-02 13:36:29 +0200

    create branch

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+    "log"
+    "os"
+    "github.com/rivo/tview"
+)
 
 func main() {
-	fmt.Println("Hello World !")
+    box := tview.NewBox()
+	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
+		panic(err)
+	}
+
+    button := tview.NewButton("OK").SetSelectedFunc(func() {
+        println("ok")
+    })
+
+	files, err := os.ReadDir(".")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    for _, file := range files {
+        if file.Type().IsDir() {
+            fmt.Println("\033[34m "+file.Name()+"/\033[37m")
+        }else{
+            fmt.Println(file.Name())
+        }
+    }
 }
